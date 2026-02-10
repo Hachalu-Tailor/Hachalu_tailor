93cb32aea01ce60ecab70940ce1600d964636bdb
MagMc007 <mergashasho7@gmail.com>
2026-02-10 21:36:48 +0300

    DOC: API documented

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -54,7 +54,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, unique=True)
 
     role = models.CharField(
         max_length=20,

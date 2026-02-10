78e3de94d709b5ab89c4271c5ed30f608a8e2821
MagMc007 <mergashasho7@gmail.com>
2026-02-10 22:17:56 +0300

    Started app inventory and made modelmigrations

--- backend/HTLBE/settings.py
+++ backend/HTLBE/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     ## custom
 
     "accounts",
+    "inventory",
 ]
 
 MIDDLEWARE = [
---
+++ backend/inventory/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+# from . import views  # Import your views here later
+
+urlpatterns = [
+    # path('materials/', views.MaterialList.as_view()),
+]

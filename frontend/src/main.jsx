ff3ff2299356c2a7b9128590c4616aec710d742f
Melke27 <melkamuwako5@gmail.com>
2026-02-21 13:38:34 +0300

    Frontend: Disable StrictMode to fix framer-motion removeChild error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../src/styles/index.css'
 import App from './App.jsx'
@@ -6,12 +5,10 @@ import { AuthProvider } from './context/AuthContext'
 import { ToastProvider } from './components/Toast'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthProvider>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
-    </AuthProvider>
-  </StrictMode>,
+  <AuthProvider>
+    <ToastProvider>
+      <App />
+    </ToastProvider>
+  </AuthProvider>
 )
 

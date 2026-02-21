8f343abac86e71cf9a2cee1fc89b041b4a4f4c14
Lidetu <lidosinafanis@gmail.com>
2026-02-21 01:51:52 -0800

    updating sample data with hp images

--- frontend/src/hooks/productData.js
+++ frontend/src/hooks/productData.js
@@ -1,12 +1,15 @@
+
+import { IMAGES } from "../constants/images";
+
 export const products = [
   { 
     id: 1, 
-    category: "Men", 
+    category: "Women", 
     name: "Midnight Peak Lapel", 
     price: "ETB 8,999", 
     desc: "Super 120s Italian wool, hand-finished lapels.", 
     img: "https://i.ebayimg.com/images/g/81kAAeSwmlZowMqW/s-l1600.webp",
-    suit_type_id: 1 // Matches API suit_type requirement
+    suit_type_id: 1 
   },
   { 
     id: 2, 
@@ -14,7 +17,8 @@ export const products = [
     name: "Signature Power Suit", 
     price: "ETB 11,000", 
     desc: "Sculpted shoulders for a commanding silhouette.", 
-    img: "https://images.unsplash.com/photo-1617113930975-f9c7243ae527?q=80&w=1374",
+    // img: "https://images.unsplash.com/photo-1617113930975-f9c7243ae527?q=80&w=1374",
+    img: IMAGES.STATIC.STATIC1,
     suit_type_id: 2
   },
   { 
@@ -23,7 +27,8 @@ export const products = [
     name: "Junior Tuxedo", 
     price: "ETB 4,500", 
     desc: "Miniature classic fit for young gentlemen.", 
-    img: "https://i.ebayimg.com/images/g/orsAAOSwMi1mpUjb/s-l1600.webp",
+    // img: "https://i.ebayimg.com/images/g/orsAAOSwMi1mpUjb/s-l1600.webp",
+    img: IMAGES.STATIC.STATIC2,
     suit_type_id: 3
   },
   { 
@@ -35,13 +40,13 @@ export const products = [
     img: "https://image.kgstores.com/is/image/marketing/25-4666887-mens-suits-desktop.jpg",
     suit_type_id: 4
   },
-  { 
-    id: 5, 
-    category: "Women", 
-    name: "Executive Blazer", 
-    price: "ETB 12,500", 
-    desc: "Premium silk-lined interior.", 
-    img: "https://images.unsplash.com/photo-1594938298603-c8148c4dae35?q=80&w=1480",
-    suit_type_id: 5
-  },
+  // { 
+  //   id: 5, 
+  //   category: "Women", 
+  //   name: "Executive Blazer", 
+  //   price: "ETB 12,500", 
+  //   desc: "Premium silk-lined interior.", 
+  //   img: "https://images.unsplash.com/photo-1594938298603-c8148c4dae35?q=80&w=1480",
+  //   suit_type_id: 5
+  // },
 ];
